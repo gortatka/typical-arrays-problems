2a76c5e4b87031e350cdae810b07198e1a7e3fa5
gortatka <gtatsiana@tut.by>
2021-08-23 22:28:54 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (array == null || array.length == 0)
+        return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == null || array.length == 0)
+        return 0;
+
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (max < array[i]) max = array[i];
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == null || array.length == 0)
+        return 0;
+
+    let sum = array.reduce((a, b) => a + b, 0);
+    let avg = sum / array.length;
+    return avg;
 }
